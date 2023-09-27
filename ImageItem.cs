c483f22d5c37fa6522ba45370ed691d5d78b08e0
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-09-27 19:30:51 +0300

    начальная загрузка картинок и форма для фильра(пустая)

---
+++ ImageItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimeProgram
+{
+    class ImageItem
+    {
+        public string Url { get; set; }
+        public string Author { get; set; }
+
+        public ImageItem(string url, string author)
+        {
+            Url = url;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return Author;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NekosBestApiNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,60 @@ namespace AnimeProgram
     /// </summary>
     public partial class MainWindow : Window
     {
+      
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        NekosBestApi _nekosBestApi;
+        List<ImageItem> _images;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //_images = new List<ImageItem>();
+            //var httpClient = new HttpClient
+            //{
+            //    BaseAddress = new Uri("https://nekos.best/api/v2")
+            //};
+            //_nekosBestApi = new NekosBestApi(httpClient);
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    var request_item = _nekosBestApi.ActionsApi.Pout().Result.Results[0];
+            //    _images.Add(new ImageItem(request_item.Url, request_item.AnimeName));
+
+            //}
+            //listImages.ItemsSource = _images;
+            _images = new List<ImageItem>();
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://nekos.best/api/v2")
+            };
+            _nekosBestApi = new NekosBestApi(httpClient);
+            for (int i = 0; i < 10; i++)
+            {
+                var request_item = _nekosBestApi.CategoryApi.Neko().Result.Results[0];
+                _images.Add(new ImageItem(request_item.Url, request_item.ArtistName));
+            }
+            listImages.ItemsSource = _images;
+
+        }
+
+        private void StackPanel_Executed(object sender, EventArgs e)
+        {
+            var panel = sender as StackPanel;
+            MessageBox.Show(panel.Children[0].ToString());
+            var me = panel.Children[0] as MediaElement;
+            me.Play();
+
+        }
+
+        private void StackPanel_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var panel = sender as StackPanel;
+
+            MediaElement me = panel.Children[0] as MediaElement;
+            me.Position = TimeSpan.FromMilliseconds(1);
+            me.Play();
         }
     }
 }
