5feff5a6f4da0ed4d48c483b0c08ed7b7cef94e1
DennisTsouris <122720690+DennisTsouris@users.noreply.github.com>
2023-10-03 19:02:25 +0300

    Finished MainWindow.xaml, InfoWindow.xaml and FilterWindow.xaml

--- FilterWindow.xaml.cs
+++ FilterWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace AnimeProgram
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for FilterWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class FilterWindow : Window
     {
-        public MainWindow()
+        public FilterWindow()
         {
             InitializeComponent();
         }
